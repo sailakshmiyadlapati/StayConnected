574d4f3ff18ccdf1d86cea72f271d7bf3c4f7db7
Sai Lakshmi Yadlapati <sailakshmiyadlapati@gmail.com>
2016-11-12 18:32:17 +0530

    changed "$url" in "connection.php"

--- functions/function.php
+++ functions/function.php
@@ -1,17 +1,15 @@
 <?php
+	global $con;
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-	$url = parse_url(getenv("mysql://b8af9b811a27ad:ed86e1b4@us-cdbr-iron-east-04.cleardb.net/heroku_2f65223a14ae42a?reconnect=true"));
-
-	$server = $url["us-cdbr-iron-east-04.cleardb.net"];
-	$username = $url["b8af9b811a27ad"];
-	$password = $url["ed86e1b4"];
-	$db = substr($url["heroku_2f65223a14ae42a"], 1);
-
-	$con = new mysqli($server, $username, $password, $db);
+	$server = $url["host"];
+	$user   = $url["user"];
+	$pass   = $url["pass"];
+	$db     = substr($url["path"], 1);
 
 	/*$conn = new mysqli($server, $username, $password, $db);*/
 
-	$con=mysqli_connect("$server", "$username", "$password", "$db") or die("Connection was not established");
+	$con=mysqli_connect("$server", "$user", "$pass", "$db") or die("Connection was not established");
 
 	
 
--- includes/connection.php
+++ includes/connection.php
@@ -1,15 +1,15 @@
 <?php
 
-$url = parse_url(getenv("mysql://b8af9b811a27ad:ed86e1b4@us-cdbr-iron-east-04.cleardb.net/heroku_2f65223a14ae42a?reconnect=true"));
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-	$server = $url["us-cdbr-iron-east-04.cleardb.net"];
-	$username = $url["b8af9b811a27ad"];
-	$password = $url["ed86e1b4"];
-	$db = substr($url["heroku_2f65223a14ae42a"], 1);
+	$server = $url["host"];
+	$user   = $url["user"];
+	$pass   = $url["pass"];
+	$db     = substr($url["path"], 1);
 
-	/*$conn = new mysqli($server, $username, $password, $db);*/
+	/*$con = new mysqli($server, $user, $pass, $db);*/
 
-	$con=mysqli_connect("$server", "$username", "$password", "$db") or die("Connection was not established");
+	$con=mysqli_connect("$server", "$user", "$pass", "$db") or die("Connection was not established");
 
 
 
